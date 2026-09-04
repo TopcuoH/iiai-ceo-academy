7b4f3bd8c75da605c14f7edaf0fbe1c37bf82f16
TopcuoH <31506421+TopcuoH@users.noreply.github.com>
2026-09-04 08:50:05 +0500

    Cache Perm Work v6 catalog update

--- perm-workspaces/sw.js
+++ perm-workspaces/sw.js
@@ -1,5 +1,5 @@
-const CACHE='perm-work-v5-20260902';
-const CORE=['./','./index.html','./styles.css','./v2.css','./v3.css','./v4.css','./data.js','./events.js','./app.js','./v2.js','./v2-fix.js','./v3.js','./v4.js','./v5.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='perm-work-v6-20260904';
+const CORE=['./','./index.html','./styles.css','./v2.css','./v3.css','./v4.css','./data.js','./events.js','./app.js','./v2.js','./v2-fix.js','./v3.js','./v4.js','./v5.js','./v6.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
